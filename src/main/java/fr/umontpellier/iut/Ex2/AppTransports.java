0ecaa035304a3a1b2aa4e3b4ae456f50bf2bb0c1
David Binaud <david.binaud@etu.umontpellier.fr>
2019-09-22 18:30:55 +0200

    Ex2 Creation package Ex2

---
+++ src/main/java/fr/umontpellier/iut/Ex2/AppTransports.java
@@ -0,0 +1,21 @@
+package fr.umontpellier.iut.Ex2;
+
+import java.util.ArrayList;
+
+public class AppTransports {
+
+    public static void main(String[] args) {
+        Wagon wagon1 = new Wagon("Rbus",110,"Toulouse");
+        Wagon wagon2 = new Wagon("LaTouristra",110,"Paris");
+        Wagon wagon3 = new Wagon("LeBonVin",90,"Bordeaux");
+        Wagon wagon4 = new Wagon("PoPoLain",145,"Blois");
+
+        ArrayList<Wagon> train = new ArrayList<>();
+        train.add(wagon1);
+        train.add(wagon2);
+        train.add(wagon3);
+        train.add(wagon4);
+
+        System.out.println(train);
+    }
+}
