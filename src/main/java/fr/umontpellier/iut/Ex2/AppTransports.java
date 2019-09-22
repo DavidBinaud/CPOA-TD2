2d0dacaabd3b579b9d4f8f47e59cadc34aee90e6
David Binaud <david.binaud@etu.umontpellier.fr>
2019-09-22 18:52:44 +0200

    Ex2 Q4 Creation Classe WagonComparatorDistPoids et test dans la classe cliente

--- src/main/java/fr/umontpellier/iut/Ex2/AppTransports.java
+++ src/main/java/fr/umontpellier/iut/Ex2/AppTransports.java
@@ -10,12 +10,14 @@ public class AppTransports {
         Wagon wagon2 = new Wagon("LaTouristra",110,"Paris");
         Wagon wagon3 = new Wagon("LeBonVin",90,"Bordeaux");
         Wagon wagon4 = new Wagon("PoPoLain",145,"Blois");
+        Wagon wagon5 = new Wagon("PoPoLain32",28,"Blois");
 
         ArrayList<Wagon> train = new ArrayList<>();
         train.add(wagon1);
         train.add(wagon2);
         train.add(wagon3);
         train.add(wagon4);
+        train.add(wagon5);
 
         System.out.println(train);
 
@@ -23,5 +25,10 @@ public class AppTransports {
         assemblagetrain.addAll(train);
 
         while(!assemblagetrain.isEmpty()) System.out.println(assemblagetrain.poll());
+        System.out.println();
+        PriorityQueue<Wagon> assemblagetrainDistPoids = new PriorityQueue<>(new WagonComparatorDistPoids());
+        assemblagetrainDistPoids.addAll(train);
+
+        while(!assemblagetrainDistPoids.isEmpty()) System.out.println(assemblagetrainDistPoids.poll());
     }
 }
