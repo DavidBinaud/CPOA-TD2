a941c3fd12122c631b2f380af07d2dff4bf11bcb
David Binaud <david.binaud@etu.umontpellier.fr>
2019-09-22 18:23:08 +0200

    Ex1 Q1 Creation de la Classe Wagon: -3 attributs -constructeur 3 parametres -toString -Getter poids

---
+++ src/main/java/fr/umontpellier/iut/Ex1/Wagon.java
@@ -0,0 +1,29 @@
+package fr.umontpellier.iut.Ex1;
+
+public class Wagon {
+
+    private String expediteur;
+
+    private int poids;
+
+    private String destination;
+
+    public Wagon(String expediteur, int poids, String destination) {
+        this.expediteur = expediteur;
+        this.poids = poids;
+        this.destination = destination;
+    }
+
+    @Override
+    public String toString() {
+        return "Wagon{" +
+                "expediteur='" + expediteur + '\'' +
+                ", poids=" + poids +
+                ", destination='" + destination + '\'' +
+                '}';
+    }
+
+    public int getPoids() {
+        return poids;
+    }
+}
