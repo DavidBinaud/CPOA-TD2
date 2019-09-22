d6f539873bf1cd02f598bcd93675dc70f6925272
David Binaud <david.binaud@etu.umontpellier.fr>
2019-09-22 21:54:43 +0200

    Ex4 Q3 Test de getTarif avec les different Tarifs sur un wagon

--- src/main/java/fr/umontpellier/iut/Ex4/AppTransports.java
+++ src/main/java/fr/umontpellier/iut/Ex4/AppTransports.java
@@ -11,5 +11,20 @@ public class AppTransports {
         Wagon wagon3 = new Wagon("LeBonVin", 90, "Bordeaux");
         Wagon wagon4 = new Wagon("PoPoLain", 145, "Blois");
 
+
+        Tarif tariffixe = new TarifFixe(1000);
+
+        Tarif tarifpoids = new TarifPoids(15);
+
+        Tarif tarifkm = new TarifKm(10);
+
+        // output 1000
+        System.out.println(wagon1.getTarif(tariffixe));
+
+        // output 15 * 110 = 1650
+        System.out.println(wagon1.getTarif(tarifpoids));
+
+        // output 10 * 249 = 2490
+        System.out.println(wagon1.getTarif(tarifkm));
     }
 }
